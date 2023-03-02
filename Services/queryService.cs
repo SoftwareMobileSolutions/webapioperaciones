81e1fd071fa2da7474b88a77993735248b770180
José María Santana Barrientos Recinos <jms_br7@hotmail.es>
2023-03-02 12:00:46 -0600

    merge

--- Services/queryService.cs
+++ Services/queryService.cs
@@ -24,7 +24,7 @@ namespace webapioperaciones.Services
                 if (conn.State == ConnectionState.Closed) {
                     conn.Open();
                 }
-                data = await conn.QueryAsync<dynamic>(query, new { idoperacion, parametros }, commandTimeout: 300, commandType: CommandType.Text);
+                data = await conn.QueryAsync<dynamic>(query, new { idoperacion, parametros }, commandTimeout: 300, commandType: CommandType.Text); //300 segundos = 5 minutos
                 if (conn.State == ConnectionState.Open) {
                     conn.Close();
                 }
