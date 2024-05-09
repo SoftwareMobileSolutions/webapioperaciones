0bb7ae338178511609d4c267ca4bf62ff9346cac
Jose Maria Barrientos <josem.barrientos@sms-open.com>
2024-05-09 09:06:17 -0600

    se deja consultav4

--- Controllers/queryController.cs
+++ Controllers/queryController.cs
@@ -22,7 +22,9 @@ namespace webapioperaciones.Controllers
             Iuser_webapi_ = _Iuser_webapi_;
         }
 
-        [HttpGet]
+
+
+        [HttpGet, HttpPost]
         [Route("{usuario}/{accesskey}/{operacionid}/{paramsvalue?}")] //a0x es el alias para llamarle a la funcion consulta
         public async Task<JsonResult> consulta(string usuario = "", string accesskey = "", string operacionid = "", string paramsvalue = "")
         {
@@ -62,6 +64,53 @@ namespace webapioperaciones.Controllers
             }
 
         }
+
+
+        [HttpGet, HttpPost]
+        [Route("consultav4")] //FROM BODY
+        public async Task<JsonResult> consultav4(string usuario = "", string accesskey = "", string operacionid = "", string paramsvalue = "")
+        {
+            try
+            {
+                bool access = (await Iuser_webapi_.getUserAccess(usuario, accesskey)).FirstOrDefault().resultado;
+                if (access)
+                {
+                    var p =
+                    paramsvalue.Split(",")//.AsParallel()
+                    .Where(x => !string.IsNullOrEmpty(x))
+                    .Select(x => x.Split(","))
+                    .Select
+                    (
+                        x => Tuple.Create
+                        (
+                            x[0].Trim().StartsWith("ff") ?
+                            "'" + x[0].Trim().Replace("ff", "").Replace("..", ":") + "'" :
+                            x[0].Trim()
+                        )
+                    )
+                     .ToList();
+
+                    string parametros = string.Join(",", p.Select(x => x.Item1));
+
+                    var data = await Iquery_.queries(operacionid, parametros);
+                    return await Task.Run(() => {
+                        return new JsonResult(data);
+                    });
+                }
+                else
+                {
+                    return new JsonResult(StatusCode(401));
+                }
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(BadRequest(ex.Message));
+            }
+
+        }
+
+
+
 
         [HttpGet]
         [Route("{usuario}/{accesskey}/{operacionid}/v2/{paramsvalue?}")] //a0x es el alias para llamarle a la funcion consulta
